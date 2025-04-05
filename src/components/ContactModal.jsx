75b45f26f174ba15e29dc5a064e5a10afbcdfe30
shoxaka <shoxaka@gmail.com>
2025-04-05 06:54:13 +0500

    modal change

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const ContactModal = ({ closeModal, isOpen }) => {
-  
+  const [formData, setFormData] = useState({
+    name: "",
+    phone: "",
+    surname: "",
+    username: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const sendMessageHandle = async (e) => {
+    e.preventDefault();
+
+    const token = "7356453117:AAGCcU1rfib6FYCKoJlt8-77Dgq5RK8UgkE";
+    const chat_id = "5625872174";
+    const URL = `https://api.telegram.org/bot${token}/sendMessage`;
+    const text = `New order for you:
+    \n Name: ${formData.name}
+    \n Phone Number: ${formData.phone}
+    \n Surname: ${formData.surname}
+    \n Username: ${formData.username}`;
+
+    try {
+      await axios.post(URL, {
+        chat_id,
+        text: text,
+      });
+
+      alert("Done my brother");
+      setFormData({
+        name: "",
+        phone: "",
+        surname: "",
+        username: "",
+      });
+      closeModal();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="fixed z-50">
@@ -54,27 +96,43 @@ const ContactModal = ({ closeModal, isOpen }) => {
               Please fill out this form.
             </h2>
 
-            <form className="space-y-4">
+            <form onSubmit={sendMessageHandle} className="space-y-4">
               <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
                 <input
                   type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
                   placeholder="Your name"
                   className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-green-500 focus:outline-none"
+                  required
                 />
                 <input
-                  type="tel"
+                  type="number"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
                   placeholder="Your phone number"
                   className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-green-500 focus:outline-none"
+                  required
                 />
                 <input
                   type="text"
+                  name="surname"
+                  value={formData.surname}
+                  onChange={handleChange}
                   placeholder="Your surname"
                   className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-green-500 focus:outline-none"
+                  required
                 />
                 <input
                   type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
                   placeholder="Your Telegram username"
                   className="w-full rounded-md border border-gray-300 px-4 py-2 focus:border-green-500 focus:outline-none"
+                  required
                 />
               </div>
 
